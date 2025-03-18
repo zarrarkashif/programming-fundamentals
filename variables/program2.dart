3b826c6a950dc0c92460b658db49fe85a22733d7
zarrarkashif <zarrarkashif56@gmail.com>
2025-03-18 22:47:27 +0500

    Program 2 done

--- variables/program2.dart
+++ variables/program2.dart
@@ -1,1 +1,24 @@
-// will start working
+// Question 2
+
+//The distance between two cities (in km.) is input through the
+//keyboard. Write a program to convert and print this distance
+//in meters, feet, inches and centimeters
+
+import 'dart:io';
+
+void main() {
+  print("Enter the distance between two cities in km :");
+  //taking input
+  double distanceCities = double.parse(stdin.readLineSync()!);
+
+//calculation
+
+  double distanceinMeters = distanceCities * 1000;
+  double distanceinFeet = distanceCities * 3280.84;
+  double distanceinInches = distanceCities * 39370.1;
+  double distanceinCentimeters = distanceCities * 100000;
+  print("The distance in Meters is: ${distanceinMeters}");
+  print("The distance in Feet is: ${distanceinFeet}");
+  print("The distance in Inches is: ${distanceinInches}");
+  print("The distance in Centimeters is: ${distanceinCentimeters}");
+}
